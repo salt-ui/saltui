1f2ce34a0a94a7b6c58d88b029b33363f1b3af4a
薛依玲 <wb-xyl259837@alibaba-inc.com>
2017-01-26 15:19:58 +0800

    update homepage

--- site/theme/template/Home.jsx
+++ site/theme/template/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import classnames from 'classnames';
+import Grid from 'uxcore-grid';
+
+const { Row, Col} = Grid;
 
 import Layout from './layout/Layout';
 import Footer from './layout/Footer';
@@ -45,27 +48,75 @@ export default (props) => (
 
     <Layout {...props} hasAside={false}>
 
-      <div className="home-banner">
-        <h2 className="brand zoomInRight animated">SaltUI</h2>
-        <p className="slogan flash animated">为企业级后台应用而生</p>
-        <Banner className="canvas" />
-      </div>
-
-      <ul className='site-feature'>
-        {
-          featureList.map((d, i) => (
-            <li className='feature-item' key={`feature-${i}`}>
-              <Icon name={d.icon} />
-              <div className='feature-item-text'>
-                <h3 className='feature-item-title'>{d.title}</h3>
-                <p className='feature-item-des'>{d.des}</p>
+      <Grid fluid={true}>
+        <Row className="home-banner">
+            <Col md={12} >
+              <h1>The top open source framework for building amazing mobile apps.
+              </h1>
+              <p>SaltUI is the beautiful, free and open source mobile UI for developing native and progressive web apps with ease.
+              </p>
+              <button className="start-btn">Get Started</button>
+            </Col>
+            <Col md={12} >
+              <div className="bg-back"></div>
+              <div className="bg-front"></div>
+            </Col>
+        </Row>
+        <Row className="site-feature">
+          <Col md={8}>
+            <div className="feature-item">
+              <div className="feature-body">  
+                <div className="feature-img img-intro"></div>
+                <h3>指南</h3>
+                <div className="intro">了解设计指南，帮助产品设计人员搭建逻辑清晰、结构合理且高效易用的产品。</div>
               </div>
-            </li>
-          ))
-        }
-      </ul>
+              <a href="#" className="feature-footer">查看详情</a>
+            </div>
+          </Col>
+          <Col md={8}>
+            <div className="feature-item">
+              <div className="feature-body">  
+                <div className="feature-img img-comp"></div>
+                <h3>组件</h3>
+                <div className="intro">使用组件 Demo 快速体验交互细节；使用前端框架封装的代码帮助工程师快速开发。</div>
+              </div>
+              <a href="#" className="feature-footer">查看详情</a>
+            </div>
+          </Col>
+          <Col md={8}>
+            <div className="feature-item">
+              <div className="feature-body">  
+                <div className="feature-img img-res"></div>
+                <h3>资源</h3>
+                <div className="intro">下载相关资源，用其快速搭建页面原型或高保真视觉稿，提升产品设计效率。</div>
+              </div>
+              <a href="#" className="feature-footer">查看详情</a>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
       <Footer />
     </Layout>
   </DocumentTitle>
 )
+
+ // <div className="home-banner">
+ //        <h2 className="brand zoomInRight animated">SaltUI</h2>
+ //        <p className="slogan flash animated">为企业级后台应用而生</p>
+ //        <Banner className="canvas" />
+ //      </div>
+
+ //      <ul className='site-feature'>
+ //        {
+ //          featureList.map((d, i) => (
+ //            <li className='feature-item' key={`feature-${i}`}>
+ //              <Icon name={d.icon} />
+ //              <div className='feature-item-text'>
+ //                <h3 className='feature-item-title'>{d.title}</h3>
+ //                <p className='feature-item-des'>{d.des}</p>
+ //              </div>
+ //            </li>
+ //          ))
+ //        }
+ //      </ul>
 
