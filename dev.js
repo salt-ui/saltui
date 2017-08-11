b1283360e9511a82940580cc06bc8851b3aff12b
光弘 <guanghong.wsj@alibaba-inc.com>
2017-08-11 19:17:23 +0800

    refactor

--- dev.js
+++
@@ -1,5 +0,0 @@
-const BiSheng = require('bisheng/lib');
-
-BiSheng.start({});
-// "bisheng": "concurrently \"bisheng start -c ./bisheng/bisheng.site.config.js --no-livereload\" \"bisheng start -c ./bisheng/bisheng.kitchen.config.js --no-livereload\"",
-// "bisheng":"bisheng start -c ./bisheng/bisheng.site.config.js",
--- site/theme/template/layout/NavLink.js
+++
@@ -1,5 +0,0 @@
-
-export default ({ href, name, route, activeLink }) => {
-	const clz = route.path === activeLink ? 'active' : ''; 
-	return <h2 className="nav-cnt"><a href={href} className={clz}>{ name }</a></h2>
-}
