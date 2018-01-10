5104a857745c103c3512f33ca0836303f0400609
MforL <1299577757@qq.com>
2018-01-10 13:49:17 +0800

    cache

--- src/Mask/index.js
+++ src/Mask/index.js
@@ -5,5 +5,6 @@
  * Copyright 2014-2016, Tingle Team, Alinw.
  * All rights reserved.
  */
+import Mask from './Mask';
 
-export default from './Mask';
+export default Mask;
--- src/NumberField/index.js
+++ src/NumberField/index.js
@@ -1,6 +1,6 @@
 /**
  * NumberField Component Style for tingle
- * @author 
+ * @author
  *
  * Copyright 2014-2017, Tingle Team, Alinw.
  * All rights reserved.
