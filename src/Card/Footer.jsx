3c9f40d1ba8e2158b24be9b1893477f30d6d3cf0
响指 <xiangzhi.wsl@alibaba-inc.com>
2018-11-27 10:14:06 +0800

    根据规则，action最多允许最多3个，移动端权重从右往左，所以会优先截取后3个，忽略其他。

--- src/Card/Footer.jsx
+++ src/Card/Footer.jsx
@@ -1,46 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { prefixClass } from '../Context';
-import Button from '../Button/Button';
-import ActionSheet from '../ActionSheet/ActionSheet'
 
-function showMore(props, context) {
-  const actions = props.actions.slice(0, -2);
-  const { locale } = context
-  ActionSheet.show({
-    options: actions.map(action => action.props.children),
-    locale
-  }, (index) => {
-    try {
-      actions[index].props.onClick()
-    } catch (err) {}
-  })
-}
-
-function ActionRender(props, context) {
-  if (!props.actions || !props.actions.length) return null;
-  const text = !context.locale || context.locale === 'zh-cn' ? '更多' : 'More';
-  return (
-    <div data-rule="fragment">
-      { <span data-rule="fragment"><Button type="minor" size="small" display="inline" onClick={() => showMore.call(this, props, context)}>{text}</Button></span> }
-      { props.actions.slice(-2).map((action, key) => <span data-rule="fragment" key={`action-${key}`}>{action}</span>) }
-    </div>
-  )
-}
-
+// 根据规则，action最多允许最多3个，移动端权重从右往左，所以会优先截取后3个，忽略其他。
 export default class Footer extends React.Component {
   render () {
     const { actions, children, content } = this.props
+    if (actions && actions.length > 3) {
+      console.error('Error from card component: Maximum 3 actions are allowed')
+    }
     return (
       children ? 
       <div className={prefixClass('card-footer')}>{children}</div> : 
       <div className={prefixClass('card-footer')}>
         <div className={prefixClass('card-footer-content')}>{content}</div>
         <div className={prefixClass('card-footer-actions')}>
-          { actions && actions.length <= 3
-          ? actions.map((action, key) => <span data-rule="fragment" key={`action-${key}`}>{action}</span>)
-          : ActionRender.call(this, this.props, this.context)
-          }
+          { actions.slice(-3).map((action, key) => <span data-rule="fragment" key={`action-${key}`}>{action}</span>) }
         </div>
       </div>
     )
