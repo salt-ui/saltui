dd69e784f459fbb842221d085a404b4a5b19746c
eternalsky <wsj7552715@hotmail.com>
2019-01-15 20:04:35 +0800

    ver. 4.3.14

--- src/Refreshcontrol/getOffset.js
+++ src/Refreshcontrol/getOffset.js
@@ -1,13 +1,19 @@
 export default function getOffset(el) {
-  const box = el.getBoundingClientRect();
-  const { body } = document;
-  const clientTop = el.clientTop || body.clientTop || 0;
-  const clientLeft = el.clientLeft || body.clientLeft || 0;
-  const scrollTop = window.pageYOffset || el.scrollTop;
-  const scrollLeft = window.pageXOffset || el.scrollLeft;
+  if (el) {
+    const box = el.getBoundingClientRect();
+    const { body } = document;
+    const clientTop = el.clientTop || body.clientTop || 0;
+    const clientLeft = el.clientLeft || body.clientLeft || 0;
+    const scrollTop = window.pageYOffset || el.scrollTop;
+    const scrollLeft = window.pageXOffset || el.scrollLeft;
 
+    return {
+      top: (box.top + scrollTop) - clientTop,
+      left: (box.left + scrollLeft) - clientLeft,
+    };
+  }
   return {
-    top: (box.top + scrollTop) - clientTop,
-    left: (box.left + scrollLeft) - clientLeft,
+    top: 0,
+    left: 0,
   };
 }
