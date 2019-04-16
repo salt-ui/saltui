1195ce3a28fbf2d2888f998acc9f20ba8b733ff1
qili.taoqili <qili.taoqili@alibaba-inc.com>
2019-04-16 11:32:45 +0800

    [ButtonGroup] add new prop fixedBottom & background for iphone x

--- src/Button/ButtonGroup.jsx
+++ src/Button/ButtonGroup.jsx
@@ -6,13 +6,38 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { prefixClass } from './utils';
 
+const disableBackground = '#f6f7f8'
+const defaultBackground = '#fff'
 class ButtonGroup extends React.Component {
+  getBackground() {
+
+    const { children, fixedBottom, background } = this.props
+    let bg = background
+    let hasBanner = false
+    children.forEach(item => {
+      const { type, disabled, display } = item.props;
+      if (type !== 'primary') {
+        if (disabled) {
+          bg = disableBackground
+        }
+      } else {
+        if (disabled) {
+          bg = defaultBackground
+        }
+      }
+      if (display === 'banner') {
+        hasBanner = true
+      }
+    });
+    return (hasBanner || fixedBottom) ? bg: 'transparent'
+  }
   render() {
     const classes = {
       [`${prefixClass('button-group')}`]: true,
+      [`fixed-bottom`]: this.props.fixedBottom
     };
     return (
-      <div className={classnames(classes)}>
+      <div className={classnames(classes)} style={{background: this.getBackground()}}>
         {this.props.children}
       </div>
     );
@@ -21,6 +46,8 @@ class ButtonGroup extends React.Component {
 
 ButtonGroup.defaultProps = {
   children: [],
+  fixedBottom: false,
+  background: defaultBackground,
 };
 
 ButtonGroup.propTypes = {
@@ -28,6 +55,8 @@ ButtonGroup.propTypes = {
     PropTypes.element,
     PropTypes.array,
   ]),
+  fixedBottom: PropTypes.bool,
+  background: PropTypes.string,
 };
 
 export default ButtonGroup;
