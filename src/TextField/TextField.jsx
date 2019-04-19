1c3caa85cf5c66b3ba8678e38d97876ef9009b45
qili.taoqili <qili.taoqili@alibaba-inc.com>
2019-04-19 11:16:29 +0800

    [TextField] fix a bug in renderView method

--- src/TextField/TextField.jsx
+++ src/TextField/TextField.jsx
@@ -73,7 +73,7 @@ class TextField extends React.Component {
             onFocus={(e) => { t.handleFocus(e); }}
             onBlur={(e) => { t.handleBlur(e); }}
           />
-        ) : <span>{renderView(t.props.value || '　')}</span>}
+        ) : <span>{renderView ? renderView(t.props.value) : (t.props.value || '　')}</span>}
       </div>
     );
   }
@@ -133,7 +133,7 @@ TextField.defaultProps = {
   value: '',
   allowClear: true,
   children: undefined,
-  renderView: value => value
+  renderView: undefined
 };
 
 TextField.propTypes = {
