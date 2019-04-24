8396904d5b69d3df4d2a61ed6e24f546ed64362f
micate <micate@me.com>
2019-04-24 19:59:25 +0800

    fix: 兼容钉钉新的人员字段

--- src/EmployeeField/utils.js
+++ src/EmployeeField/utils.js
@@ -1,12 +1,38 @@
 // 把 钉钉api 返回的值转换成 key/label 格式
 function transToValue(list) {
-  return (list || []).map(item => (
-    {
-      key: item.emplId,
-      label: item.nickNameCn || item.name,
-      avatar: item.avatar,
+  return (list || []).map((item) => {
+    const {
+      emplId,
+      nick,
+      nickNameCn,
+      name,
+      orgUserName,
+      avatar,
+    } = item;
+
+    let label;
+    if (orgUserName) {
+      // 兼容钉钉新字段
+      if (nick) {
+        label = `${orgUserName}(${nick})`;
+      } else {
+        label = orgUserName;
+      }
+    } else if (name) {
+      // 原有逻辑，调整为 姓名(花名) 的方式
+      if (nickNameCn && nickNameCn !== name) {
+        label = `${name}(${nickNameCn})`;
+      } else {
+        label = name;
+      }
     }
-  ));
+
+    return {
+      key: emplId,
+      label,
+      avatar,
+    };
+  });
 }
 
 export default { transToValue };
